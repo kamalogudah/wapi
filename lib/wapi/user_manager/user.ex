b45dcad4f8a5ab9c7db2e9c168953a3028f7b6ad
kamalogudah <mcpaul2058@gmail.com>
2019-05-22 16:24:15 +0300

    Password Hashing

--- lib/wapi/user_manager/user.ex
+++ lib/wapi/user_manager/user.ex
@@ -1,6 +1,7 @@
 defmodule Wapi.UserManager.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Comeonin.Bcrypt
 
   schema "users" do
     field :password, :string
@@ -14,5 +15,14 @@ defmodule Wapi.UserManager.User do
     user
     |> cast(attrs, [:username, :password])
     |> validate_required([:username, :password])
+    |> put_password_hash()
   end
+
+  defp put_password_hash(
+         %Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset
+       ) do
+    change(changeset, password: Bcrypt.hashpwsalt(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
